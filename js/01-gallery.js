eac1cd1e79008a94b12bb457918b6c7a3e2b7930
wxoox2000 <wxoox2000@gmail.com>
2023-06-23 04:28:31 +0300

    update files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+list.insertAdjacentHTML("beforeend", markup);
+let instance;
+list.addEventListener("click", handlerClick);
+function handlerClick(evt) {
+  let isTarget = evt.target.classList.contains("gallery__image");
+  if (!isTarget) {
+    return;
+  }
+  evt.preventDefault();
+  let originalImage = evt.target.dataset.source;
+  instance = basicLightbox.create(`
+    <img src="${originalImage}" width="800" height="600">
+`);
+  instance.show();
+  const visible = instance.visible();
+    if (visible) {
+      document.addEventListener("keydown", handleKey);
+      function handleKey(evt) {
+        if (evt.code === "Escape") {
+          instance.close();
+        }
+      }
+    }
+    else {
+      document.removeEventListener("keydown", handleKey);
+    }
+  }
 
-console.log(galleryItems);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+list.insertAdjacentHTML("beforeend", markup);
+const gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+list.addEventListener("click", handlerClick);
+function handlerClick(evt) {
+  let isTarget = evt.target.classList.contains("gallery__image");
+  if (!isTarget) {
+    return;
+  }
+  evt.preventDefault();
+}
 
-console.log(galleryItems);
+// console.log(lightbox);
